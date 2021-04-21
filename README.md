cf0eb14321d07e48d1eebbabac5016b8ba022cbf
emilylondon <elondon@usc.edu>
2021-04-21 01:58:19 -0700

    YouTube video search & download configured

--- README.md
+++ README.md
@@ -4,3 +4,21 @@ EE250 Final Project
 Install yt-dl 
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+
+Open venv
+run: 
+python3 -m venv ./venv
+source venv/bin/activate
+pip3 install youtube_dl
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip
+deactivate
+
+Install ffmpeg
+sudo apt-get install ffmpeg
+and macOS use the command:
+brew install ffmpeg
+
+Install Certificate Command: 
+run: 
+/Applications/Python\ 3.7/Install\ Certificates.command
--- ytDownload.py
+++ ytDownload.py
@@ -1,6 +1,22 @@
 from __future__ import unicode_literals
-import youtube_dl
+from youtube_dl import YoutubeDL
+from apiclient.discovery import build
+import os
+import json
 
+#API key for YouTube Search API
+apikey = "AIzaSyBMfPWXD-sXoNLRZCUiYCknKrGApSicxwc"
+youtube = build('youtube', 'v3', developerKey=apikey)
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -9,6 +25,40 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
+    'logger': MyLogger(),
 }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=uJ480TCCMAs'])
+
+while True: 
+    searchTerm= input('Enter search query:  ')
+    req = youtube.search().list(q=searchTerm, part='snippet', type = 'video', maxResults=5)
+    res = req.execute()
+
+    for i in range(5):
+        print(str(i+1) + ": " + res['items'][i]['snippet']['title'])
+
+    select = int(input('Enter number to select song: '))
+    ytUrl="https://www.youtube.com/watch?v=" + res['items'][select-1]['id']['videoId']
+
+    try:
+        print('Youtube Downloader'.center(40, '_'))
+        
+        URL = ytUrl
+        
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([URL])
+        os.rename(res['items'][select-1]['snippet']['title']+"-"+res['items'][select-1]['id']['videoId']+".mp3", "newSong.mp3")
+        os.system("afplay " + "newSong.mp3")
+
+
+    
+    except Exception:
+      
+        print("Couldn\'t download the audio")
+    
+    finally:
+      
+        option = int(input('\n1.download again \n2.Exit\n\nOption here :'))
+        
+        if option!=1:
+        
+            break
