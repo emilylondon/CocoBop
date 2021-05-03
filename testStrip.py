efada1853f9ece24b4089dc60aaf7ef11fb6d95f
emilylondon <elondon@usc.edu>
2021-05-03 00:44:29 -0700

    Testing to see if variable max brightness works

--- testStrip.py
+++ testStrip.py
@@ -6,24 +6,25 @@ import pigpio
 import time 
 
 pi = pigpio.pi()
+audio_max = 255
 
 while True:
-    for r in range(255):
+    for r in range(audio_max):
         pi.set_PWM_dutycycle(RED_PIN, r)
         time.sleep(0.005)
-    for b in range(255, 0, -1):
+    for b in range(audio_max, 0, -1):
         pi.set_PWM_dutycycle(BLUE_PIN, b)
         time.sleep(0.005)
-    for g in range(255):
+    for g in range(audio_max):
         pi.set_PWM_dutycycle(GREEN_PIN, g)
         time.sleep(0.005)
-    for r in range(255, 0, -1):
+    for r in range(audio_max, 0, -1):
         pi.set_PWM_dutycycle(RED_PIN, r)
         time.sleep(0.005)
-    for b in range(255):
+    for b in range(audio_max):
         pi.set_PWM_dutycycle(BLUE_PIN, b)
         time.sleep(0.005)
-    for g in range(255, 0, -1):
+    for g in range(audio_max, 0, -1):
         pi.set_PWM_dutycycle(GREEN_PIN, g)
         time.sleep(0.005)
     
